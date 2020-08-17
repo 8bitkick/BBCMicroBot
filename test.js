0005a0e4142b89f874b03e95cb06671d1a165b91
Dominic Pajak <8bitkick@users.noreply.github.com>
2020-08-16 22:14:37 -0700

    tests end marker

--- test.js
+++ test.js
@@ -10,8 +10,8 @@ function Tests(since_id){
     {
       name: "FRAME_CAPTURE", // MODE 0-6
       text: "0 MODE 2\n10 FOR C = 0 TO 7\n20 COLOUR C\n30 PRINT \"COLOUR \",C\n40 NEXT C\n"+
-            "60 MOVE 0,0\n70 DRAW 1279,0\n80 DRAW 1279,1023\n90 DRAW 0,1023\n100 DRAW 0,0\n"+
-            "110 DRAW 1279,1023\n120 VDU 23,1,0;0;0;0;\n130 P.TAB(0,16);INT(TIME/10)/10;\" s   \"\n140 GOTO 130",
+      "60 MOVE 0,0\n70 DRAW 1279,0\n80 DRAW 1279,1023\n90 DRAW 0,1023\n100 DRAW 0,0\n"+
+      "110 DRAW 1279,1023\n120 VDU 23,1,0;0;0;0;\n130 P.TAB(0,16);INT(TIME/10)/10;\" s   \"\n140 GOTO 130",
       mediaType: "video/mp4",
       checksum: "16fcadcb6bb3b10db6f196683b6ed9129187d692"
     },
@@ -78,69 +78,69 @@ function Tests(since_id){
       mediaType: "image/png",
       checksum: "a7bffbe345411afb00e58f032df1800a8f02d663"
     },
-      {name: null, text: null}
+    {name: null, text: "TESTS_END"}
   ]
 }
 
-  // Get the next section of timeline timeline
-  Tests.prototype.update = async function () {}
+// Get the next section of timeline timeline
+Tests.prototype.update = async function () {}
 
-  // Add the interesting timeline to the BBC Emulator queue
-  Tests.prototype.pop = function (timeline) {
+// Add the interesting timeline to the BBC Emulator queue
+Tests.prototype.pop = function (timeline) {
 
-    var test = this.tests.shift();
-    var tweet = {
-      'created_at'                : null,
-      'user'                      : {'screen_name':"<TEST SERVER>"},
-      'text'                      : test.text,
-      'id_str'                    : test.name,
-      'in_reply_to_status_id_str' : "1",
-      'truncated'                 : false,
-      'favorited'                 : false,
-      'user_mentions'             : ['bbcmicrobot'],
-      'entities'                  : {user_mentions: ["test"]},
-      'bbcmicrobot_has_audio'     : (test.hasAudio == true),
-      'bbcmicrobot_checksum'      : test.checksum,
-      'bbcmicrobot_media_type'    : test.mediaType
-    };
+  var test = this.tests.shift();
+  var tweet = {
+    'created_at'                : null,
+    'user'                      : {'screen_name':"<TEST SERVER>"},
+    'text'                      : test.text,
+    'id_str'                    : test.name,
+    'in_reply_to_status_id_str' : "1",
+    'truncated'                 : false,
+    'favorited'                 : false,
+    'user_mentions'             : ['bbcmicrobot'],
+    'entities'                  : {user_mentions: ["test"]},
+    'bbcmicrobot_has_audio'     : (test.hasAudio == true),
+    'bbcmicrobot_checksum'      : test.checksum,
+    'bbcmicrobot_media_type'    : test.mediaType
+  };
 
-    return tweet;
-  }
+  return tweet;
+}
 
-  function exec(cmd) {
-    const exec = require('child_process').exec;
-    return new Promise((resolve, reject) => {
-      exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-          throw error;
-        }
-        resolve(stdout? stdout.trim() : stderr);
-      });
+function exec(cmd) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      resolve(stdout? stdout.trim() : stderr);
     });
-  }
+  });
+}
 
-  function videoReply(filename,mediaType,replyTo,text,tweet,checksum,hasAudio){
-    console.log("checksum: "+checksum)
-    if (tweet.bbcmicrobot_checksum != checksum) {
-      throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
-    }
-    console.log("mediaType: "+mediaType)
-    if (tweet.bbcmicrobot_media_type != mediaType) {
-      throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
-    }
-    console.log("hasAudio: "+hasAudio)
-    if (tweet.bbcmicrobot_has_audio != hasAudio) {
-      throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
-    }
-    if (mediaType == 'video/mp4') {
-      exec('ffprobe -v 0 -select_streams a -show_streams '+filename).then(
-        function(audioInfo) {
-          var videoHasAudio = (audioInfo.length > 0);
-          console.log("videoHasAudio: "+videoHasAudio);
-          if (hasAudio != videoHasAudio) {
-            throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
-          }
-        });
+function videoReply(filename,mediaType,replyTo,text,tweet,checksum,hasAudio){
+  console.log("checksum: "+checksum)
+  if (tweet.bbcmicrobot_checksum != checksum) {
+    throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
+  }
+  console.log("mediaType: "+mediaType)
+  if (tweet.bbcmicrobot_media_type != mediaType) {
+    throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
+  }
+  console.log("hasAudio: "+hasAudio)
+  if (tweet.bbcmicrobot_has_audio != hasAudio) {
+    throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
+  }
+  if (mediaType == 'video/mp4') {
+    exec('ffprobe -v 0 -select_streams a -show_streams '+filename).then(
+      function(audioInfo) {
+        var videoHasAudio = (audioInfo.length > 0);
+        console.log("videoHasAudio: "+videoHasAudio);
+        if (hasAudio != videoHasAudio) {
+          throw new Error(replyTo+' TEST - \u001b[31mFAILED\u001b[0m')
+        }
+      });
     }
     console.log(replyTo+' TEST - \u001b[32mOK\u001b[0m')
   }
