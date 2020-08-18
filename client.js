b6bb5c6718a30f1557b15575c168c3e983cfbd5f
Dominic Pajak <8bitkick@users.noreply.github.com>
2020-08-18 16:13:37 -0700

    Strawman of POST video to bot server

--- client.js
+++ client.js
@@ -7,21 +7,20 @@ var   POLL_DELAY  = process.env.POLL_DELAY || 5000
 const TEST = (process.argv.indexOf("test") > -1)
 POLL_DELAY = TEST ? 0 : POLL_DELAY ;
 
-const emulationDuration   = 33; // seconds
-const startFrame          = 1587; // frames (50fps when vsync active)
+const emulationDuration   = 3; //33; // seconds
+const startFrame          = 0; //1587; // frames (50fps when vsync active)
 
 const fs           = require('fs');
-const requirejs    = require('requirejs'); // for jsbeeb compatibility
 const https        = require('https');
+const requirejs    = require('requirejs'); // for jsbeeb compatibility
+const request      = require('request');
 const cert_path    = "./certs/";
-
 
 var twtr = require(TEST ? './test' : './tweet');
 
 var botServer = {
   hostname: HOST,
   port: PORT,
-  method: 'GET',
   key: fs.readFileSync(cert_path+'client_key.pem'),
   cert: fs.readFileSync(cert_path+'client_cert.pem'),
   ca: fs.readFileSync(cert_path+'server_cert.pem')
@@ -59,101 +58,126 @@ requirejs(['emulator'],
 function (emulator) {
   "use strict";
 
-    async function run(tweet){
-      if (TEST && tweet.text=="TESTS_END"){
-         process.exit();
+  async function run(tweet){
+
+    if (TEST && tweet.text=="TESTS_END"){
+      process.exit();
+    }
+
+    console.log("");
+    console.log("Running "+tweet.id_str+" from @"+tweet.user.screen_name);
+
+    var path = "./tmp/"+tweet.id_str;
+
+    // Tweet ID will be used in tmp filename passed into shell exec, so check it's safe.  For a real tweet it should be numeric while for a testcase it can contain alphanumerics.
+    if (/\W/.test(tweet.id_str)) {
+      console.error("id_str contained unexpected character");
+      process.exit();
+    }
+
+    // Run tweet on emulator
+    var start   = new Date()
+    var frames  = await emulator.emulate(tweet.text,path,emulationDuration,startFrame);
+    var end     = new Date() - start
+    console.log("JSbeeb DONE in %ds ",end/1000);
+
+    // Count unique video frames
+    var uniqueFrames = (await exec("shasum "+path+"*.rgba | awk '{print $1}' | sort | uniq | wc -l"));
+
+    console.log("JSbeeb captured "+frames+" frames ("+uniqueFrames+" unique)");
+
+    start = new Date()
+
+    var hasAudio = fs.existsSync(path+"audiotrack.raw");
+
+    if (frames == 0) {
+      // NO VIDEO -> NOTHING
+      var ffmpegCmd = "";
+    } else if (uniqueFrames==1 && !hasAudio) {
+      // STATIC IMAGE WITHOUT SOUND -> PNG SCREENSHOT
+      var mediaFilename = path+'.png';
+      var mediaType = 'image/png';
+      var ffmpegCmd = 'ffmpeg -hide_banner -y -f rawvideo -pixel_format rgba -video_size 640x512  -i '+path+'frame'+(frames-1)+'.rgba -vf "scale=1280:1024" '+mediaFilename
+    } else {
+      // ANIMATION OR STATIC IMAGE WITH SOUND -> MP4 VIDEO
+      var mediaFilename = path+'.mp4';
+      var mediaType = 'video/mp4';
+      var ffmpegCmd = 'ffmpeg -hide_banner -loglevel panic ';
+      if (hasAudio) {
+        ffmpegCmd = ffmpegCmd + '-f f32le -ar 44100 -ac 1 -i '+path+'audiotrack.raw ';
       }
+      ffmpegCmd = ffmpegCmd + '-y -f image2 -r 50 -s 640x512 -pix_fmt rgba -vcodec rawvideo -i '+path+'frame%d.rgba  -af "highpass=f=50, lowpass=f=15000,volume=0.5" -filter:v "scale=1280:1024" -q 0 -b:v 8M -b:a 128k -c:v libx264 -pix_fmt yuv420p -strict -2 -shortest '+mediaFilename
+    }
 
-      console.log("");
-      console.log("Running "+tweet.id_str+" from @"+tweet.user.screen_name);
+    if (frames > 0) {
+      await exec(ffmpegCmd);
+      var checksum = await exec('shasum '+path+'frame'+(frames-1)+'.rgba'+" | awk '{print $1}'");
+    } else {
+      var checksum = '';
+    }
+    exec('rm -f '+path+'*.rgba '+path+'*.raw');
 
-      var path = "./tmp/"+tweet.id_str;
+    var end = new Date() - start
+    console.log("Ffmpeg DONE in %ds ",end/1000);
 
-      // Tweet ID will be used in tmp filename passed into shell exec, so check it's safe.  For a real tweet it should be numeric while for a testcase it can contain alphanumerics.
-      if (/\W/.test(tweet.id_str)) {
-        console.error("id_str contained unexpected character");
-        process.exit();
-      }
+    if (frames == 0) {
+      //  twtr.noOutput(tweet);
+    } else {
+      //  twtr.videoReply(mediaFilename,mediaType,tweet.id_str,"@"+tweet.user.screen_name,tweet,checksum,hasAudio);
+    }
 
-      // Run tweet on emulator
-      var start   = new Date()
-      var frames  = await emulator.emulate(tweet.text,path,emulationDuration,startFrame);
-      var end     = new Date() - start
-      console.log("JSbeeb DONE in %ds ",end/1000);
+    await sendVideo();
 
-      // Count unique video frames
-      var uniqueFrames = (await exec("shasum "+path+"*.rgba | awk '{print $1}' | sort | uniq | wc -l"));
+    setTimeout(requestTweet, 5000);
+  };
 
-      console.log("JSbeeb captured "+frames+" frames ("+uniqueFrames+" unique)");
+  function requestTweet() {
+    var options = botServer;
+    console.log("Requesting tweet")
+    options.uri="https://"+HOST+":"+PORT+"/pop";
+    options.method="GET";
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+        var tweet = JSON.parse(body);
 
-      start = new Date()
+        if (typeof tweet.text === 'undefined') {
+          setTimeout(requestTweet, 5000);
+          return;
 
-      var hasAudio = fs.existsSync(path+"audiotrack.raw");
-
-      if (frames == 0) {
-        // NO VIDEO -> NOTHING
-        var ffmpegCmd = "";
-      } else if (uniqueFrames==1 && !hasAudio) {
-        // STATIC IMAGE WITHOUT SOUND -> PNG SCREENSHOT
-        var mediaFilename = path+'.png';
-        var mediaType = 'image/png';
-        var ffmpegCmd = 'ffmpeg -hide_banner -y -f rawvideo -pixel_format rgba -video_size 640x512  -i '+path+'frame'+(frames-1)+'.rgba -vf "scale=1280:1024" '+mediaFilename
-      } else {
-        // ANIMATION OR STATIC IMAGE WITH SOUND -> MP4 VIDEO
-        var mediaFilename = path+'.mp4';
-        var mediaType = 'video/mp4';
-        var ffmpegCmd = 'ffmpeg -hide_banner -loglevel panic ';
-        if (hasAudio) {
-          ffmpegCmd = ffmpegCmd + '-f f32le -ar 44100 -ac 1 -i '+path+'audiotrack.raw ';
+        } else {
+          if (TEST && tweet.text == null) {process.exit()};
+          run(tweet).catch((err) => console.error(err));;
         }
-        ffmpegCmd = ffmpegCmd + '-y -f image2 -r 50 -s 640x512 -pix_fmt rgba -vcodec rawvideo -i '+path+'frame%d.rgba  -af "highpass=f=50, lowpass=f=15000,volume=0.5" -filter:v "scale=1280:1024" -q 0 -b:v 8M -b:a 128k -c:v libx264 -pix_fmt yuv420p -strict -2 -shortest '+mediaFilename
       }
-
-      if (frames > 0) {
-        await exec(ffmpegCmd);
-        var checksum = await exec('shasum '+path+'frame'+(frames-1)+'.rgba'+" | awk '{print $1}'");
-      } else {
-        var checksum = '';
-      }
-      exec('rm -f '+path+'*.rgba '+path+'*.raw');
-
-      var end = new Date() - start
-      console.log("Ffmpeg DONE in %ds ",end/1000);
-
-      if (frames == 0) {
-        twtr.noOutput(tweet);
-      } else {
-        twtr.videoReply(mediaFilename,mediaType,tweet.id_str,"@"+tweet.user.screen_name,tweet,checksum,hasAudio);
-      }
-
-      setTimeout(requestTweet, POLL_DELAY);
-    };
-
-    function requestTweet() {
-      botServer.path="/pop";
-      https.get(botServer, (resp) => {
-        let data = '';
-        resp.setEncoding('utf8');
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-          var tweet = JSON.parse(data);
-          if (typeof tweet.text === 'undefined') {
-            setTimeout(requestTweet, 5000);
-            return;
-
-          } else {
-            if (TEST && tweet.text == null) {process.exit()};
-            run(tweet).catch((err) => console.error(err));;
-          }
-        });
-      }).on("error", (err) => {
-        log("Error: " + err.message);
+      if (error){
+        log("Error: " + error.message);
         log('Retry in 5 seconds');
         setTimeout(requestTweet, 5000);
-      });
+      }
+    })
+  }
+
+  async function sendVideo() {
+    var post_data = JSON.stringify({ data: 'somedata' })
+    var options = botServer;
+    options.method="POST";
+    options.path="/video/";
+    options.uri="https://"+HOST+":"+PORT+"/video";
+    options.timeout=3000;
+    options.body=post_data;
+    options.headers= {
+      'Content-Type': 'application/json',
+      'Content-Length': post_data.length
+    };
+
+    request.post(options, post_data,
+      function(err,httpResponse,body){
+        if (err) {
+          return console.error('upload failed:', err);
+        }
+        console.log('Post successful:', body);
+      })
     }
 
     var try_arg = process.argv.indexOf("try") + 1;
