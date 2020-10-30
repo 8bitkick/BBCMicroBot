2341a0a2e3d912c495d02e3cd65aaf1c374011b5
Dominic Pajak <8bitkick@users.noreply.github.com>
2020-10-30 11:46:45 -0700

    Add (dormant) beebjit support

--- client.js
+++ client.js
@@ -15,7 +15,7 @@ const fs           = require('fs');
 const requirejs    = require('requirejs'); // for jsbeeb compatibility
 const https        = require('https');
 const base2048     = require('base2048');
-const cert_path    = "./certs/"; 
+const cert_path    = "./certs/";
 
 const Filter       = require('bad-words');
 const customFilter = new Filter({ placeHolder: '*'});
@@ -60,12 +60,12 @@ if (cluster.isMaster && MP == 'true') {
       console.log("Emulator process exited");
     })
   });
-
+  
 } else {
-
+  
   if (cluster.isWorker) {clientID = "Cli"+(cluster.worker.id-1);}
   require( 'console-stamp' )( console, { pattern: 'dd/mm/yyyy HH:MM:ss '},clientID+":" );
-
+  
   requirejs.config({
     baseUrl: "./node_modules/jsbeeb/",
     paths: {
@@ -75,14 +75,14 @@ if (cluster.isMaster && MP == 'true') {
       'emulator':'../../emulator'
     }
   });
-
+  
   requirejs(['emulator'],
   function (emulator) {
     "use strict";
-
+    
     function processInput(tweet) {
       var i = tweet.text;
-
+      
       function b2048decode(text)  {
         const regex = /^.*🗜(.*)/gu;
         var result = regex.exec(text);
@@ -98,11 +98,11 @@ if (cluster.isMaster && MP == 'true') {
         }
         return text;
       }
-
+      
       function remove_first_occurrence(str, searchstr)       {
         var index = str.indexOf(searchstr); if (index === -1) {return str;}
         return str.slice(0, index) + str.slice(index + searchstr.length);}
-
+        
         var userMentions = [];
         if (typeof tweet.entities.user_mentions != 'undefined' ){
           tweet.entities.user_mentions.forEach(function(m) {
@@ -113,52 +113,74 @@ if (cluster.isMaster && MP == 'true') {
             }
           });
         }
-
+        
         userMentions.forEach(function(m) {
           i = i.slice(0, m[0]) + i.slice(m[1]);
         });
-
+        
         i = b2048decode(i.trim());
-
+        
         i = i.replace(/[“]/g,'"');
         i = i.replace(/[”]/g,'"');
         i = i.replace(/&lt;/g,'<');
         i = i.replace(/&gt;/g,'>');
         i = i.replace(/&amp;/g,'&');
- 
+        
         return i;
       }
-
-
+      
+      
       async function run(tweet){
         console.log("");
         console.log("Running "+tweet.id_str+" from @"+tweet.user.screen_name);
-
+        
         // Convert tweet to BBC Micro friendly characters
         var input = processInput(tweet);
-        var path = "./tmp/"+tweet.id_str;
-
+        var beebjit = false;
+        
+        var start   = new Date()
+        
+        // Emulate
+        if (beebjit == true) {
+          var path = "./beebjit/";
+          var prefix = "beebjit_frame_";
+          var pixel_format = "bgra";
+          var emu_name = "beebjit";
+          var beebjit_flags = "-fast -accurate -headless -autoboot -opt video:border-chars=0 -rom 7 roms/gxr.rom -cycles 17300000000 -frame-cycles 7200000000 -max-frames 200";
+          
+          // Run tweet on emulator
+          await fs.writeFileSync("./beebasm/text.bas",input+"\rRUN\r");
+          await exec("cd beebasm && ./beebasm -i makedisk.asm -do tweet.ssd -opt 3 && cd ../beebjit");
+          await exec(" cd beebjit && ./beebjit -0 ../beebasm/tweet.ssd "+beebjit_flags+" && cd ..");
+          
+        } else // JSbeeb
+        {
+          var path = "./tmp/"+tweet.id_str;
+          var prefix = "frame";
+          var pixel_format = "rgba";
+          var emu_name = "jsbeeb";
+          var frames  = await emulator.emulate(input,path,emulationDuration,startFrame);
+        }
+        
         // Tweet ID will be used in tmp filename passed into shell exec, so check it's safe.  For a real tweet it should be numeric while for a testcase it can contain alphanumerics.
         if (/\W/.test(tweet.id_str)) {
           console.error("id_str contained unexpected character");
           process.exit();
         }
-
-        // Run tweet on emulator
-        var start   = new Date()
-        var frames  = await emulator.emulate(input,path,emulationDuration,startFrame);
+        
         var end     = new Date() - start
-        console.log("JSbeeb DONE in %ds ",end/1000);
-
+        console.log(emu_name+" DONE in %ds ",end/1000);
+        
         // Count unique video frames
+        var frames = (await exec("shasum "+path+"*.rgba | awk '{print $1}' | wc -l"));
         var uniqueFrames = (await exec("shasum "+path+"*.rgba | awk '{print $1}' | sort | uniq | wc -l"));
-
-        console.log("JSbeeb captured "+frames+" frames ("+uniqueFrames+" unique)");
-
+        
+        console.log("Captured "+frames+" frames ("+uniqueFrames+" unique)");
+        
         start = new Date()
-
+        
         var hasAudio = fs.existsSync(path+"audiotrack.raw");
-
+        
         if (frames == 0) {
           // NO VIDEO -> NOTHING
           var ffmpegCmd = "";
@@ -166,7 +188,7 @@ if (cluster.isMaster && MP == 'true') {
           // STATIC IMAGE WITHOUT SOUND -> PNG SCREENSHOT
           var mediaFilename = path+'.png';
           var mediaType = 'image/png';
-          var ffmpegCmd = 'ffmpeg -hide_banner -y -f rawvideo -pixel_format rgba -video_size 640x512  -i '+path+'frame'+(frames-1)+'.rgba -vf "scale=1280:1024" '+mediaFilename
+          var ffmpegCmd = 'ffmpeg -hide_banner -y -f rawvideo -pixel_format '+pixel_format+' -video_size 640x512  -i '+path+prefix+(frames-1)+'.rgba -vf "scale=1280:1024" '+mediaFilename
         } else {
           // ANIMATION OR STATIC IMAGE WITH SOUND -> MP4 VIDEO
           var mediaFilename = path+'.mp4';
@@ -175,20 +197,20 @@ if (cluster.isMaster && MP == 'true') {
           if (hasAudio) {
             ffmpegCmd = ffmpegCmd + '-f f32le -ar 44100 -ac 1 -i '+path+'audiotrack.raw ';
           }
-          ffmpegCmd = ffmpegCmd + '-y -f image2 -r 50 -s 640x512 -pix_fmt rgba -vcodec rawvideo -i '+path+'frame%d.rgba  -af "highpass=f=50, lowpass=f=15000,volume=0.5" -filter:v "scale=1280:1024" -q 0 -b:v 8M -b:a 128k -c:v libx264 -pix_fmt yuv420p -strict -2 -shortest '+mediaFilename
+          ffmpegCmd = ffmpegCmd + '-y -f image2 -r 50 -s 640x512 -pix_fmt '+pixel_format+' -vcodec rawvideo -i '+path+prefix+'%d.rgba  -af "highpass=f=50, lowpass=f=15000,volume=0.5" -filter:v "scale=1280:1024" -q 0 -b:v 8M -b:a 128k -c:v libx264 -pix_fmt yuv420p -strict -2 -shortest '+mediaFilename
         }
-
+        
         if (frames > 0) {
           await exec(ffmpegCmd);
-          var checksum = await exec('shasum '+path+'frame'+(frames-1)+'.rgba'+" | awk '{print $1}'");
+          var checksum = await exec('shasum '+path+pixel_format+(frames-1)+'.rgba'+" | awk '{print $1}'");
         } else {
           var checksum = '';
         }
         exec('rm -f '+path+'*.rgba '+path+'*.raw');
-
+        
         var end = new Date() - start
         console.log("Ffmpeg DONE in %ds ",end/1000);
-
+        
         if (customFilter.clean(input) != input) {
           console.warn("BLOCKED @"+tweet.user.screen_name)
           twtr.block(tweet);
@@ -197,65 +219,65 @@ if (cluster.isMaster && MP == 'true') {
         } else {
           twtr.videoReply(mediaFilename,mediaType,tweet.id_str,"@"+tweet.user.screen_name,tweet,checksum,hasAudio);
         }
-
+        
         setTimeout(requestTweet, POLL_DELAY);
       };
-
-          function requestTweet() {
-            tweetServer.path="/pop";
-            https.get(tweetServer, (resp) => {
-              let data = '';
-              resp.setEncoding('utf8');
-              resp.on('data', (chunk) => {
-                data += chunk;
-              });
-              // The whole response has been received. Print out the result.
-              resp.on('end', () => {
-                var tweet = JSON.parse(data);
-                if (typeof tweet.text === 'undefined') {
-                  setTimeout(requestTweet, 5000);
-                  return;
-
-                } else {
-                  if (TEST && tweet.text == null) {process.exit()};
-                  run(tweet).catch((err) => console.error(err));;
-                }
-              });
-            }).on("error", (err) => {
-              log("Error: " + err.message);
-              log('Retry in 5 seconds');
+      
+      function requestTweet() {
+        tweetServer.path="/pop";
+        https.get(tweetServer, (resp) => {
+          let data = '';
+          resp.setEncoding('utf8');
+          resp.on('data', (chunk) => {
+            data += chunk;
+          });
+          // The whole response has been received. Print out the result.
+          resp.on('end', () => {
+            var tweet = JSON.parse(data);
+            if (typeof tweet.text === 'undefined') {
               setTimeout(requestTweet, 5000);
-            });
-          }
-
-          var try_arg = process.argv.indexOf("try") + 1;
-          if (try_arg > 0) {
-            var try_file = (try_arg == process.argv.length) ? "/dev/stdin" : process.argv[try_arg];
-            var tweet = {
-              text: fs.readFileSync(try_file, 'utf8'),
-              id_str: 'try',
-              user: { screen_name: 'try' },
-              entities: {}
-            };
-            // Set up twtr object to mock the 'tweet' methods that we use.
-            twtr = {};
-            twtr.videoReply = function(filename,mediaType,replyTo,text,tweet,checksum,hasAudio) {
-              console.log("Generated " + mediaType);
-              exec("xdg-open "+filename);
-              process.exit();
-            };
-            twtr.block = function(tweet) {
-              console.log("Failed: Tweet blocked because of badwords");
-              process.exit();
-            };
-            twtr.noOutput = function(tweet) {
-              console.log("Failed: No output captured");
-              process.exit();
-            };
-            run(tweet);
-          } else {
-            requestTweet();
-          }
-        }
-      );
+              return;
+              
+            } else {
+              if (TEST && tweet.text == null) {process.exit()};
+              run(tweet).catch((err) => console.error(err));;
+            }
+          });
+        }).on("error", (err) => {
+          log("Error: " + err.message);
+          log('Retry in 5 seconds');
+          setTimeout(requestTweet, 5000);
+        });
+      }
+      
+      var try_arg = process.argv.indexOf("try") + 1;
+      if (try_arg > 0) {
+        var try_file = (try_arg == process.argv.length) ? "/dev/stdin" : process.argv[try_arg];
+        var tweet = {
+          text: fs.readFileSync(try_file, 'utf8'),
+          id_str: 'try',
+          user: { screen_name: 'try' },
+          entities: {}
+        };
+        // Set up twtr object to mock the 'tweet' methods that we use.
+        twtr = {};
+        twtr.videoReply = function(filename,mediaType,replyTo,text,tweet,checksum,hasAudio) {
+          console.log("Generated " + mediaType);
+          exec("xdg-open "+filename);
+          process.exit();
+        };
+        twtr.block = function(tweet) {
+          console.log("Failed: Tweet blocked because of badwords");
+          process.exit();
+        };
+        twtr.noOutput = function(tweet) {
+          console.log("Failed: No output captured");
+          process.exit();
+        };
+        run(tweet);
+      } else {
+        requestTweet();
+      }
     }
+  );
+}
