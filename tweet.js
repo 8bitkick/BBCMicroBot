ce75d2dd9941463509079968ebbd82d873009538
Dominic Pajak <8bitkick@users.noreply.github.com>
2020-11-03 12:37:30 -0800

    Post image to discord

--- tweet.js
+++ tweet.js
@@ -61,7 +61,7 @@ const discordClient = new Discord.WebhookClient(process.env.webhookID, process.e
     console.log("Media post DONE ");
 
     // Post to discord too
-    var content = text+" posted \n\n`"+input+"`\nhttps://www.twitter.com/bbcmicrobot/status/"+response.id_str;
+    var content = text+" posted \n\n`"+input+"`\n"+response.entities.media[0].media_url_https;
     console.log(content);
     discordClient.send('Webhook test', {
 	username: 'bbcmicrobot',
