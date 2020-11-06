a45e23172aed6ef6cb486ca61ef150abda4d41d7
Olly Betts <olly@survex.com>
2020-11-07 11:23:46 +1300

    Restore detokenisation of code sent to discord

--- tweet.js
+++ tweet.js
@@ -15,6 +15,19 @@
 
   const twitter          = new Twitter(API_KEYS);
 
+  function detokenize(string){
+    const tokens = ["AND ","DIV ","EOR ","MOD ","OR ","ERROR ","LINE ","OFF ","STEP ","SPC","TAB(","ELSE ","THEN ","line no. ","OPENIN","PTR","PAGE ","TIME ","LOMEM ","HIMEM ","ABS","ACS","ADVAL","ASC","ASN","ATN","BGET","COS","COUNT ","DEG","ERL ","ERR ","EVAL","EXP","EXT","FALSE ","FN","GET ","INKEY","INSTR","INT","LEN","LN","LOG","NOT ","OPENIN","OPENOUT","PI ","POINT(","POS ","RAD","RND","SGN","SIN","SQR","TAN","TO ","TRUE ","USR","VAL","VPOS ","CHR$","GET$ ","INKEY$","LEFT$(","MID$(","RIGHT$(","STR$","STRING$(","EOF ","AUTO ","DELETE ","LOAD ","LIST ","NEW ","OLD ","RENUMBER ","SAVE ","PUT","PTR","PAGE ","TIME ","LOMEM ","HIMEM ","SOUND ","BPUT","CALL ","CHAIN ","CLEAR ","CLOSE","CLG ","CLS ","DATA ","DEF ","DIM ","DRAW ","END ","ENDPROC ","ENVELOPE ","FOR ","GOSUB ","GOTO ","GCOL ","IF ","INPUT ","LET ","LOCAL ","MODE ","MOVE ","NEXT ","ON ","VDU ","PLOT ","PRINT ","PROC","READ ","REM ","REPEAT ","REPORT ","RESTORE ","RETURN ","RUN ","STOP ","COLOUR ","TRACE ","UNTIL ","WIDTH ","OSCLI"];
+
+    var graphemes = splitter.splitGraphemes(string.trim());
+    var output = "";
+
+    for (let i = 0; i<graphemes.length; i++){
+      var g = graphemes[i].codePointAt(0) & 0xff;
+      output += g>=0x80 ? " "+tokens[g-0x80] : graphemes[i];
+    }
+    return output;
+  }
+
   function post (endpoint, params) {
     return new Promise((resolve, reject) => {
       twitter.post(endpoint, params, (error, data, response) => {
@@ -56,7 +69,7 @@
     console.log("Media post DONE ");
 
     // Post to discord too
-    var content = "["+text+"](https://www.twitter.com/"+response.in_reply_to_screen_name+">) posted \n```basic\n"+input+"```\n"+response.entities.media[0].media_url_https+"\n[See original tweet](https://www.twitter.com/bbcmicrobot/status/"+response.id_str+">)\n";
+    var content = "["+text+"](https://www.twitter.com/"+response.in_reply_to_screen_name+">) posted \n```basic\n"+detokenise(input)+"```\n"+response.entities.media[0].media_url_https+"\n[See original tweet](https://www.twitter.com/bbcmicrobot/status/"+response.id_str+">)\n";
     console.log(content);
 
     if (typeof discordClient === 'undefined') {
